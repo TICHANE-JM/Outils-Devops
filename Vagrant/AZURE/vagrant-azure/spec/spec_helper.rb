b1e4fdd07f9e7d97892213668833b3d5436dd1b9
TICHANE Jean-Michel <107214400+TICHANE-JM@users.noreply.github.com>
2022-06-28 15:36:54 +0200

    Update spec_helper.rb

--- Vagrant/AZURE/vagrant-azure/spec/spec_helper.rb
+++ Vagrant/AZURE/vagrant-azure/spec/spec_helper.rb
@@ -1,10 +1,10 @@
 # encoding: utf-8
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See License in the project root for license information.
+# Copyright (c) Microsoft Corporation. Tous les droits sont réservés.
+# Sous licence sous la licence MIT. Voir Licence dans la racine du projet pour les informations de licence.
 
 require 'vagrant-azure'
 
-# import all the support files
+# importer tous les fichiers de support
 Dir[File.join(File.dirname(__FILE__), 'support/**/*.rb')].each { |f| require File.expand_path(f) }
 
 RSpec.configure do |config|
