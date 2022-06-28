14a611455e211d2d9b751cf4329d4636a1ac640b
TICHANE Jean-Michel <107214400+TICHANE-JM@users.noreply.github.com>
2022-06-28 12:16:31 +0200

    Update read_ssh_info.rb

--- Vagrant/AWS/vagrant-aws-master/lib/vagrant-aws/action/read_ssh_info.rb
+++ Vagrant/AWS/vagrant-aws-master/lib/vagrant-aws/action/read_ssh_info.rb
@@ -3,8 +3,8 @@ require "log4r"
 module VagrantPlugins
   module AWS
     module Action
-      # This action reads the SSH info for the machine and puts it into the
-      # `:machine_ssh_info` key in the environment.
+      # Cette action lit les informations SSH de la machine et les place la clé
+      # `:machine_ssh_info` de l'environnement.
       class ReadSSHInfo
         def initialize(app, env)
           @app    = app
@@ -20,22 +20,22 @@ module VagrantPlugins
         def read_ssh_info(aws, machine)
           return nil if machine.id.nil?
 
-          # Find the machine
+          # Trouver l'appareil
           server = aws.servers.get(machine.id)
           if server.nil?
-            # The machine can't be found
+            # La machine est introuvable
             @logger.info("Machine couldn't be found, assuming it got destroyed.")
             machine.id = nil
             return nil
           end
 
-          # read attribute override
+          # remplacement d'attribut de lecture
           ssh_host_attribute = machine.provider_config.
               get_region_config(machine.provider_config.region).ssh_host_attribute
-          # default host attributes to try. NOTE: Order matters!
+          # attributs d'hôte par défaut à essayer. REMARQUE : la commande est importante !
           ssh_attrs = [:dns_name, :public_ip_address, :private_ip_address]
           ssh_attrs = (Array(ssh_host_attribute) + ssh_attrs).uniq if ssh_host_attribute
-          # try each attribute, get out on first value
+          # essayez chaque attribut, sortez sur la première valeur
           host_value = nil
           while !host_value and attr_name = ssh_attrs.shift
             begin
